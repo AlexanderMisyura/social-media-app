498e838e4710b30633dc1ef53af524d6cc61207b
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-10-01 20:56:44 +0300

    initial routing and view

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const hbs = require("express-handlebars").engine;
 
-require("dotenv").config({path: "./config/.env"})
+require("dotenv").config({ path: "./config/.env" });
 
 connectDB();
+
+// Handlebars
+app.engine(
+  ".hbs",
+  hbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+
+// Routes
+app.use("/", require("./routes/indexRoute"));
 
 const PORT = process.env.PORT || 3000;
 
