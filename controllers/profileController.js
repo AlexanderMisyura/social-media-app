11093f59cf3a6c2b4a1d14deccd39544e119fd8d
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-10-08 15:35:43 +0300

    add profile basic styling

---
+++ controllers/profileController.js
@@ -0,0 +1,22 @@
+const CommentSchema = require("../models/CommentSchema");
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+module.exports = {
+  getProfile: async (req, res) => {
+    try {
+      console.log(req.user.id === req.params.id); // true
+      const user = await User.findOne({ _id: req.user.id });
+      const posts = await Post.find({ user: req.user.id });
+      const comments = await CommentSchema.find({ user: req.user.id });
+      console.log(user);
+      console.log("---------");
+      console.log(posts);
+      console.log("---------");
+      console.log(comments);
+      res.render("profile");
+    } catch (err) {
+      console.error(err);
+    }
+  },
+};
