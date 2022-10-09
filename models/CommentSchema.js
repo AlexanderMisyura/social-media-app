6a64e3da371bf9a862ac1543a4cc16058e40652a
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-10-10 01:01:29 +0300

    renew comment schema

--- models/CommentSchema.js
+++ models/CommentSchema.js
@@ -4,10 +4,16 @@ const CommentSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    required: true,
   },
   post: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post",
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
   },
   likes: {
     type: Number,
