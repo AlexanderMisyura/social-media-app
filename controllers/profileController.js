c0ba502425432573afdd98f11b1e7a8b4b7c896e
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-10-10 03:09:54 +0300

    profile page populates with user info from DB (except posts)

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -5,11 +5,32 @@ const User = require("../models/User");
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      console.log(req.user.id === req.params.id); // true
-      const user = await User.findOne({ _id: req.user.id });
-      const posts = await Post.find({ user: req.user.id });
-      const comments = await CommentSchema.find({ user: req.user.id });
-      res.render("profile");
+      let posts;
+      let comments;
+      let user = await User.findOne({ id: req.params.id }).lean();
+      if (req.user.id === req.params.id) {
+        posts = await Post.find({ user: req.user.id, deleted: false })
+          .populate("user")
+          .sort({ creationDate: "desc" })
+          .lean();
+        comments = {
+          bodies: await CommentSchema.find({ user: req.user.id }).lean(),
+          count: await CommentSchema.count({ user: req.user.id }),
+        };
+      } else {
+        posts = await Post.find({
+          user: req.user.id,
+          status: "public",
+          deleted: false,
+        })
+          .populate("user")
+          .sort({ creationDate: "desc" })
+          .lean();
+        comments = {
+          count: await CommentSchema.count({ user: req.params.id }),
+        };
+      }
+      res.render("profile", { posts, comments, user, title: `${user.userName}'s profile` });
     } catch (err) {
       console.error(err);
     }
