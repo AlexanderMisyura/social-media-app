4bccccf1a30ab38eefc4909c2dcec12b56dfb12b
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-10-11 02:36:42 +0300

    add ability to write posts
    Store images in cloudinary, use multer to deal with file uploads.

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,10 +1,15 @@
 const Post = require("../models/Post");
+const cloudinary = require("../middleware/cloudinary");
+const mongoose = require("mongoose");
 
 module.exports = {
   getIndex: async (req, res) => {
     try {
-      const posts = await Post.find().sort({ creationDate: "desc" }).populate("user").lean();
-      res.render("index", { title: "Socister | Feed", posts});
+      const posts = await Post.find()
+        .sort({ creationDate: "desc" })
+        .populate("user")
+        .lean();
+      res.render("index", { title: "Socister | Feed", posts });
     } catch (err) {
       console.error(err);
     }
@@ -12,5 +17,30 @@ module.exports = {
 
   getAddPost: (req, res) => {
     res.render("posts/add", { title: "Socister | Create an awsome new post" });
+  },
+
+  savePost: async (req, res) => {
+    try {
+      let post = await Post.create({
+        title: req.body.title,
+        caption: req.body.caption,
+        user: mongoose.Types.ObjectId(req.user.id),
+        status: req.body.status,
+      });
+
+      const postId = post._id.toString();
+      const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+        folder: `socister/posts/${req.user.id}/${postId}`,
+      });
+
+      post.image = uploadResult.secure_url;
+      post.cloudinaryId = uploadResult.public_id;
+      await post.save();
+      console.log(post);
+
+      res.redirect(`/profile/${req.user.id}`);
+    } catch (err) {
+      console.error(err);
+    }
   },
 };
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,7 +1,9 @@
 const postsController = require("../controllers/postsController");
 const { ensureAuth } = require("../middleware/auth");
-router = require("express").Router();
+const router = require("express").Router();
+const upload = require("../middleware/multer");
 
 router.get("/add", ensureAuth, postsController.getAddPost);
+router.post("/add", ensureAuth, upload.single("image"), postsController.savePost);
 
 module.exports = router;
