9186700253f539698cd2b1ba44edb680f9feb4f7
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-10-16 21:44:18 +0300

    add missing post route handler

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ app.use(flash());
 // Routes
 app.use("/", require("./routes/indexRoute"));
 app.use("/profile", require("./routes/profileRoute"));
+app.use("/post", require("./routes/postRoute"));
 app.get("*", (req, res) => {
   res.render("error/404", { layout: "narrow", title: "404 NOT FOUND" });
 });
