9e6e833413918fd501fe187f158e6615508898a4
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-10-16 21:51:30 +0300

    fix issue with form data
    Form data was not sending to server.
    Use multer middleware, add missing name attributes to hbs file,
    also fix image upload and add missing icon.

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,10 +1,13 @@
 const profileController = require("../controllers/profileController");
 const { ensureAuth } = require("../middleware/auth");
 const router = require("express").Router();
+const upload = require("../middleware/multer");
 
 // @desc Show a single user's profile (own or another user)
 // @route GET /profile/:userId
 router.get("/:id", ensureAuth, profileController.getProfile);
 router.get("/settings/:id", ensureAuth, profileController.getProfileSettings);
+router.post("/", ensureAuth, upload.single("image"), profileController.saveProfileChanges);
+// router.get("/passwordChange", ensureAuth, profileController.);
 
 module.exports = router;
