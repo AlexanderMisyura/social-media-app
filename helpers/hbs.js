073d771c316527df26ed84cde58da230ebdf3d26
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-10-18 19:42:40 +0300

    add icons to post bottom

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -17,17 +17,46 @@ module.exports = {
       let post = posts[i];
       let parentTile = `<div class="tile is-parent is-6">
                           <div class="tile box is-child is-max-width-100">
-                            <article class="media">
+                            <article class="media is-align-items-stretch">
                               <figure class="media-left">
-                                <p class="image is-128x128">
+                                <p class="image is-128x128 is-96x96-mobile">
                                   <img src="${post.image}">
                                 </p>
                               </figure>
-                              <div class="media-content is-cropped">
-                              <div class="">
+                              <div class="media-content is-cropped is-flex is-flex-direction-column is-justify-content-space-between">
+                              <div class="content">
                                 <h4 class="title is-5 is-cropped">${post.title}</h4>
                                 <p class="subtitle is-6 is-cropped">${post.caption}</p>
                               </div>
+                              <footer class="is-flex is-justify-content-space-between">
+                              <div>
+                              <span class="icon has-text-grey">
+                                <i class="fa-regular fa-eye fa-lg"></i>
+                              </span>
+                              </div>
+                              <div>
+                              <span class="icon has-text-dark">
+                                <i class="fa-solid fa-comment fa-lg"></i>
+                              </span>
+                              </div>
+                              <div>
+                                <a class="pr-1" href="">
+                                  <span class="icon has-text-dark">
+                                    <i class="fa-solid fa-share-nodes fa-lg"></i>
+                                  </span>
+                                </a>
+                                <a class="pr-1" href="">
+                                  <span class="icon has-text-dark">
+                                    <i class="fa-regular fa-bookmark fa-lg"></i>
+                                  </span>
+                                </a>
+                                <a href="">
+                                  <span class="icon has-text-dark">
+                                    <i class="fa-regular fa-star fa-lg"></i>
+                                  </span>
+                                </a>
+                              </div>
+                              </footer>
                               </div>
                             </article>
                           </div>
