364398d439d01c0c98c2ce305a690376d0097d2a
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-11-02 12:12:09 +0300

    fix comment router

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -2,6 +2,6 @@ const commentController = require("../controllers/commentController");
 const { ensureAuth } = require("../middleware/auth");
 const router = require("express").Router();
 
-router.post("/:postId", commentController.saveComment);
+router.post("/", ensureAuth, commentController.saveComment);
 
 module.exports = router;
