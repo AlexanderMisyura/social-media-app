cafc2c3bb1e3f8abea99bfe07df710f46cc8143b
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-11-02 12:08:41 +0300

    comments can be stored in db

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,11 +3,17 @@ const CommentSchema = require("../models/CommentSchema");
 module.exports = {
   saveComment: async (req, res) => {
     try {
-      console.log(req.body)
       req.body.user = req.user.id;
-      req.body.post = req.params.postId;
-      console.log(req.body);
-      res.json(req.body)
+      const comment = await CommentSchema.create(req.body);
+      if (!comment) {
+        return console.error(error);
+      }
+      if (comment.replyTo) {
+        await CommentSchema.findByIdAndUpdate(req.body.replyTo, {
+          $inc: { replies: 1 },
+        });
+      }
+      res.json(req.body);
     } catch (error) {
       console.error(err);
       res.render("error/500", {
@@ -15,5 +21,5 @@ module.exports = {
         title: "500 SOMETHING WENT WRONG",
       });
     }
-  }
-}
+  },
+};
