7e71c0bc05c9999fe41174a4690ac3bb157dfeb1
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-11-04 14:40:31 +0300

    add client side bookmark management

---
+++ public/js/bookmark.js
@@ -0,0 +1,36 @@
+if (document.querySelector(".bookmark")) {
+  class Bookmark {
+    constructor(element) {
+      this.bookmarkElement = element;
+    }
+
+    async #toggleBookmark() {
+      try {
+        let resPromise = await fetch(
+          `/bookmarks/${this.bookmarkElement.dataset.postId}`,
+          {
+            method: "PUT",
+          }
+        );
+        let res = await resPromise.json();
+        if (res) {
+          this.bookmarkElement.classList.toggle("has-text-success");
+          this.bookmarkElement.classList.toggle("has-text-dark");
+          document.querySelector(".bookmarkCounter").textContent =
+            res.bookmarks || 0;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    listen() {
+      this.bookmarkElement.addEventListener("click", () => this.#toggleBookmark());
+    }
+  }
+
+  for (let bookmarkElement of document.querySelectorAll(".bookmark")) {
+    let bookmark = new Bookmark(bookmarkElement);
+    bookmark.listen();
+  }
+}
