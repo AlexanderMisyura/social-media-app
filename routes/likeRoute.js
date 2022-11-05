8f117a1ad21450f0509663a029489333f669a0f8
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-11-05 12:30:32 +0300

    add ensure auth middleware to like route

--- routes/likeRoute.js
+++ routes/likeRoute.js
@@ -2,6 +2,6 @@ const likeController = require("../controllers/likeController");
 const { ensureAuth } = require("../middleware/auth");
 const router = require("express").Router();
 
-router.put("/post/:postId", likeController.toggleLike);
+router.put("/post/:postId", ensureAuth, likeController.toggleLike);
 
 module.exports = router;
