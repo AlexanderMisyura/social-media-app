98c4a03bba3aaf7c7b777af06c63aa8dd277c316
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-11-17 15:00:57 +0300

    enhance comment deletion
    Controller recursively reduces the amount of replies of
    deleted comment's parents.

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -56,22 +56,24 @@ module.exports = {
 
   deleteComment: async (req, res) => {
     try {
-      const comment = await CommentSchema.findById(req.params.commentId, {
-        user: 1,
-        post: 1,
-      });
+      const comment = await CommentSchema.findOne(
+        { _id: req.params.commentId, user: req.user.id, deleted: false },
+        {
+          user: 1,
+          post: 1,
+          replyTo: 1,
+          replies: 1,
+        }
+      );
 
       if (!comment) {
         // The user has already deleted the comment
         // or the query parameter (comment id) was incorrect
+        // or user tries to send a query to delete someone else's comment
         return res.render("error/404", {
           layout: "narrow",
           title: "404 NOT FOUND",
         });
-      }
-      if (req.user.id !== comment.user.toString()) {
-        // User somehow send a query to delete someone else's comment
-        return res.redirect("/");
       }
 
       // Mark comment as deleted
@@ -79,13 +81,30 @@ module.exports = {
         { _id: req.params.commentId },
         { deleted: true }
       );
-      if (!update.acknowledged) {
+      if (update.acknowledged) {
+        // Recursively cheks parent comments and reduce their replies if needed
+        async function checkParents(comment) {
+          if (comment.replyTo && !comment.replies) {
+            const parent = await CommentSchema.findOneAndUpdate(
+              { _id: comment.replyTo },
+              { $inc: { replies: -1 } },
+              { new: true, projection: { replies: 1, replyTo: 1, deleted: 1 } }
+            );
+            if (parent.deleted) {
+              checkParents(parent);
+            }
+          }
+        }
+        checkParents(comment);
+      } else {
         return res.render("error/404", {
           layout: "narrow",
           title: "404 NOT FOUND",
         });
       }
+
       res.redirect("back");
+
     } catch (error) {
       console.error(error);
       res.render("error/500", {
