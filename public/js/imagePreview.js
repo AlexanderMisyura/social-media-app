3ce2ff0556294171e96723c43f405052703f8eac
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-12-14 14:38:18 +0300

    add image upload preview

---
+++ public/js/imagePreview.js
@@ -0,0 +1,10 @@
+if (document.getElementById("preview")) {
+  console.log('"preview"', "preview")
+  const imageInput = document.querySelector(".file-input");
+  imageInput.addEventListener("change", previewImage);
+  function previewImage(e) {
+    const preview = document.getElementById("preview");
+    preview.src = URL.createObjectURL(e.target.files[0]);
+    preview.addEventListener("load", () => URL.revokeObjectURL(preview.src));
+  }
+}
