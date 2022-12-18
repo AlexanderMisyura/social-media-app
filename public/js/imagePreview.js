80ff107bff91c0f2768746af77cf2519636d388f
Alexander Misiura <87127837+AlexanderMisyura@users.noreply.github.com>
2022-12-18 19:04:32 +0300

    merge js scripts into one single file

--- public/js/imagePreview.js
+++
@@ -1,9 +0,0 @@
-if (document.getElementById("preview")) {
-  const imageInput = document.querySelector(".file-input");
-  imageInput.addEventListener("change", previewImage);
-  function previewImage(e) {
-    const preview = document.getElementById("preview");
-    preview.src = URL.createObjectURL(e.target.files[0]);
-    preview.addEventListener("load", () => URL.revokeObjectURL(preview.src));
-  }
-}
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,42 @@
+if (document.querySelector(".bookmark")) {
+  class Bookmark {
+    constructor(element) {
+      this.bookmarkElement = element;
+    }
+
+    async #toggleBookmark() {
+      try {
+        let resPromise = await fetch(
+          `/bookmarks/${this.bookmarkElement.dataset.postId}`,
+          {
+            method: "PUT",
+          }
+        );
+        let res = await resPromise.json();
+        if (res) {
+          this.bookmarkElement.classList.toggle("has-text-success");
+          this.bookmarkElement.classList.toggle("has-text-dark");
+          document.querySelector(".bookmarkCounter").textContent =
+            res.bookmarks || 0;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    listen() {
+      this.bookmarkElement.addEventListener("click", () =>
+        this.#toggleBookmark()
+      );
+    }
+  }
+
+  for (let bookmarkElement of document.querySelectorAll(".bookmark")) {
+    let bookmark = new Bookmark(bookmarkElement);
+    bookmark.listen();
+  }
+}
+
 if (document.querySelector("#commentSection")) {
   // Add show/hide reply form to each comment
   function toggleReplyForm(e) {
@@ -151,4 +190,139 @@ if (document.querySelector("#commentSection")) {
       btnCommentBranch.addEventListener("click", () => commentBranch.toggle());
     });
   }
+}
+
+if (document.getElementById("preview")) {
+  const imageInput = document.querySelector(".file-input");
+  imageInput.addEventListener("change", previewImage);
+  function previewImage(e) {
+    const preview = document.getElementById("preview");
+    preview.src = URL.createObjectURL(e.target.files[0]);
+    preview.addEventListener("load", () => URL.revokeObjectURL(preview.src));
+  }
+}
+
+class Like {
+  constructor(element) {
+    this.likeElement = element;
+  }
+
+  async #toggleLike() {
+    const route = this.likeElement.dataset.isCommentLike
+      ? `/like/comment/${this.likeElement.dataset.commentId}`
+      : `/like/post/${this.likeElement.dataset.postId}`;
+    try {
+      const resPromise = await fetch(route, {
+        method: "PUT",
+      });
+      const res = await resPromise.json();
+      if (res) {
+        this.likeElement.classList.toggle("has-text-warning");
+        this.likeElement.classList.toggle("has-text-dark");
+        this.likeElement.parentElement.querySelector(
+          ".likeCounter"
+        ).textContent = res.likes || 0;
+        if (document.querySelector("#userRating")) {
+          document.querySelector("#userRating").textContent = res.userRating;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  listen() {
+    this.likeElement.addEventListener("click", () => this.#toggleLike());
+  }
+}
+
+for (let likeElement of document.querySelectorAll(".like")) {
+  let like = new Like(likeElement);
+  like.listen();
+}
+
+// Functions to open and close a modal
+function openModal(modalElement) {
+  modalElement.classList.add("is-active");
+}
+
+function closeModal(modalElement) {
+  modalElement.classList.remove("is-active");
+}
+
+function closeAllModals() {
+  (document.querySelectorAll(".modal") || []).forEach((modalElement) => {
+    closeModal(modalElement);
+  });
+}
+
+// Add a click event on buttons to open a specific modal
+(document.querySelectorAll(".js-modal-trigger") || []).forEach(
+  (modalTriggerOpen) => {
+    const targetName = modalTriggerOpen.dataset.targetName;
+    const modalElement = document.getElementById(targetName);
+
+    modalTriggerOpen.addEventListener("click", () => {
+      openModal(modalElement);
+    });
+  }
+);
+
+// Add a click event on various child elements to close the parent modal
+(
+  document.querySelectorAll(
+    ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+  ) || []
+).forEach((modalTriggerClose) => {
+  const modalElement = modalTriggerClose.closest(".modal");
+
+  modalTriggerClose.addEventListener("click", () => {
+    closeModal(modalElement);
+  });
+});
+
+// Add a keyboard event to close all modals
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    // Escape key
+    closeAllModals();
+  }
+});
+
+const navbarBurger = document.querySelector(".navbar-burger");
+navbarBurger.addEventListener("click", (e) => {
+  const targetName = e.currentTarget.dataset.targetName;
+  const navbarMenu = document.getElementById(targetName);
+
+  navbarBurger.classList.toggle("is-active");
+  navbarMenu.classList.toggle("is-active");
+});
+
+if (document.querySelector(".js-tab-trigger")) {
+  const tabTriggers = document.querySelectorAll(".js-tab-trigger");
+  const tabContentBlocks = document.querySelectorAll(".tab");
+
+  const updateTabs = (newActiveTabTrigger) => {
+    tabTriggers.forEach((tabTrigger) =>
+      tabTrigger.classList.remove("is-active")
+    );
+    newActiveTabTrigger.classList.add("is-active");
+  };
+
+  const updateContent = (activeTrigger) => {
+    const activeContentBlock = document.getElementById(
+      activeTrigger.dataset.tab
+    );
+    tabContentBlocks.forEach((block) => {
+      block.classList.add("is-hidden");
+    });
+    activeContentBlock.classList.remove("is-hidden");
+  };
+
+  tabTriggers.forEach((tabTrigger) => {
+    tabTrigger.addEventListener("click", (e) => {
+      updateTabs(e.currentTarget);
+      updateContent(e.currentTarget);
+    });
+  });
 }
