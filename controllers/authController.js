954a9aa6b21ebfc5b8f625546e2722cba7957ce2
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2023-12-26 23:04:24 +0300

    feat: improve login and signup input validation
    Use flash middleware to send errors to the redirect page when invalid
    credentials are entered. Separate errors into categories for correct
    display near the corresponding field. Add user name validation.

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,23 +4,42 @@ const User = require("../models/User");
 
 module.exports = {
   getLogin: (req, res) => {
-    res.render("login", { layout: "narrow", title: "Socister | Login" });
+    res.render("login", {
+      layout: "narrow",
+      title: "Socister | Login",
+      validationErrors: req.flash("validationErrors"),
+      authErrors: req.flash("authErrors"),
+    });
   },
 
   getSignup: (req, res) => {
-    res.render("signup", { layout: "narrow", title: "Socister | Signup" });
+    res.render("signup", {
+      layout: "narrow",
+      title: "Socister | Signup",
+      validationErrors: req.flash("validationErrors"),
+      duplicationErrors: req.flash("duplicationErrors"),
+    });
   },
 
   postLogin: (req, res, next) => {
-    const validationErrors = [];
+    const validationErrors = { emailErrors: [], passwordErrors: [] };
 
-    if (!validator.isEmail(req.body.email))
-      validationErrors.push({ msg: "Please, enter a valid email address." });
-    if (validator.isEmpty(req.body.password))
-      validationErrors.push({ msg: "Password cannot be blank." });
+    if (!validator.isEmail(req.body.email)) {
+      validationErrors.emailErrors.push({
+        msg: "Please, enter a valid email address",
+      });
+    }
+    if (validator.isEmpty(req.body.password)) {
+      validationErrors.passwordErrors.push({
+        msg: "Password cannot be blank",
+      });
+    }
 
-    if (validationErrors.length) {
-      req.flash("errors", validationErrors);
+    if (
+      validationErrors.emailErrors.length ||
+      validationErrors.passwordErrors.length
+    ) {
+      req.flash("validationErrors", validationErrors);
       return res.redirect("/login");
     }
 
@@ -33,7 +52,7 @@ module.exports = {
         return next(err);
       }
       if (!user) {
-        req.flash("errors", info);
+        req.flash("authErrors", info);
         return res.redirect("/login");
       }
       req.logIn(user, (err) => {
@@ -47,19 +66,37 @@ module.exports = {
   },
 
   postSignup: (req, res, next) => {
-    const validationErrors = [];
-    if (!validator.isEmail(req.body.email))
-      validationErrors.push({ msg: "Please enter a valid email address." });
-    if (!validator.isLength(req.body.password, { min: 8 }))
-      validationErrors.push({
+    const validationErrors = {
+      userNameErrors: [],
+      emailErrors: [],
+      passwordErrors: [],
+    };
+
+    if (!validator.isLength(req.body.userName, { min: 3, max: 30 })) {
+      validationErrors.userNameErrors.push({
+        msg: "Username must be a minimum of 3 and a maximum of 30 characters long",
+      });
+    }
+    if (!validator.isEmail(req.body.email)) {
+      validationErrors.emailErrors.push({
+        msg: "Please enter a valid email address",
+      });
+    }
+    if (!validator.isLength(req.body.password, { min: 8 })) {
+      validationErrors.passwordErrors.push({
         msg: "Password must be at least 8 characters long",
       });
-    if (req.body.password !== req.body.confirmPassword)
-      validationErrors.push({ msg: "Passwords do not match" });
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+      validationErrors.passwordErrors.push({ msg: "Passwords do not match" });
+    }
 
-    if (validationErrors.length) {
-      req.flash("errors", validationErrors);
-      return res.redirect("../signup");
+    if (
+      validationErrors.emailErrors.length ||
+      validationErrors.passwordErrors.length
+    ) {
+      req.flash("validationErrors", validationErrors);
+      return res.redirect("/signup");
     }
     req.body.email = validator.normalizeEmail(req.body.email, {
       gmail_remove_dots: false,
@@ -78,10 +115,10 @@ module.exports = {
           return next(err);
         }
         if (existingUser) {
-          req.flash("errors", {
-            msg: "Account with that email address or username already exists.",
+          req.flash("duplicationErrors", {
+            msg: "Account with this email address or username already exists",
           });
-          return res.redirect("../signup");
+          return res.redirect("/signup");
         }
         user.save((err) => {
           if (err) {
